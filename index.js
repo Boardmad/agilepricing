6d756a3560bc759673c37d787e713329a7c8fc29
Scott Brown <scottbrown@boardmad.com>
2020-07-08 16:42:09 +0100

    good commits

--- index.js
+++ index.js
@@ -9,15 +9,22 @@ window.onload = function() {
 }
 
 function callback(data) {		
-		dataPoints.push(data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href);
-		dataPoints.push(data.single_register_electricity_tariffs._B.direct_debit_monthly.links[1].href);
-		dataPoints.push(data.single_register_electricity_tariffs._C.direct_debit_monthly.links[1].href);
-		dataPoints.push(data.single_register_electricity_tariffs._D.direct_debit_monthly.links[1].href);
-		dataPoints.push(data.single_register_electricity_tariffs._E.direct_debit_monthly.links[1].href);
-		dataPoints.push(data.single_register_electricity_tariffs._F.direct_debit_monthly.links[1].href);
+		dataPoints.push(data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._B.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._C.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._D.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._E.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._F.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._G.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._H.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._J.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._K.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._M.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._N.direct_debit_monthly.links[1].href + "<br />");
+		dataPoints.push(data.single_register_electricity_tariffs._P.direct_debit_monthly.links[1].href + "<br />");
+		
+		//alert(dataPoints.join());
 
-		alert(dataPoints.join.toString());
-
-		$("#divTarrifs").html(datapoints.join()).css("background-color", "orange");
+		$("#divTarrifs").html(dataPoints.join());
 
 }

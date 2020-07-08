9c85513c31bcce784cec2acab25c4ef44e824d47
Scott Brown <scottbrown@boardmad.com>
2020-07-08 20:48:10 +0100

    consilifating interface

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var dataPoints = [];
+var tariffDPS = [];
 
 window.onload = function() {
 
@@ -9,22 +9,22 @@ window.onload = function() {
 }
 
 function callback(data) {		
-		dataPoints.push(data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._B.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._C.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._D.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._E.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._F.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._G.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._H.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._J.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._K.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._M.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._N.direct_debit_monthly.links[1].href + "<br />");
-		dataPoints.push(data.single_register_electricity_tariffs._P.direct_debit_monthly.links[1].href + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._A.direct_debit_monthly.code, data.single_register_electricity_tariffs._A.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._B.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._B.direct_debit_monthly.code, data.single_register_electricity_tariffs._B.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._C.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._C.direct_debit_monthly.code, data.single_register_electricity_tariffs._C.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._D.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._D.direct_debit_monthly.code, data.single_register_electricity_tariffs._D.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._E.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._E.direct_debit_monthly.code, data.single_register_electricity_tariffs._E.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._F.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._F.direct_debit_monthly.code, data.single_register_electricity_tariffs._F.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._G.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._G.direct_debit_monthly.code, data.single_register_electricity_tariffs._G.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._H.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._H.direct_debit_monthly.code, data.single_register_electricity_tariffs._H.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._J.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._J.direct_debit_monthly.code, data.single_register_electricity_tariffs._J.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._K.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._K.direct_debit_monthly.code, data.single_register_electricity_tariffs._K.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._L.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._L.direct_debit_monthly.code, data.single_register_electricity_tariffs._L.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._M.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._M.direct_debit_monthly.code, data.single_register_electricity_tariffs._M.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._N.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._N.direct_debit_monthly.code, data.single_register_electricity_tariffs._N.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
+	tariffDPS.push(data.single_register_electricity_tariffs._P.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._P.direct_debit_monthly.code, data.single_register_electricity_tariffs._P.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
 		
-		//alert(dataPoints.join());
-
-		$("#divTarrifs").html(dataPoints.join());
+	/* uncomment to debug */
+	$("#divTariffs").html(tariffDPS.join(''));
 
 }

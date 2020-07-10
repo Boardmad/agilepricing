6694c86ed1a29f5e0bc2b94cc8d6867e7736bfc6
Scott Brown <scottbrown@boardmad.com>
2020-07-10 18:14:16 +0100

    consolidated single page app

---
+++ getTariffs.js
@@ -0,0 +1,99 @@
+
+var chart = null;
+var tariffDPS = [];
+var dataPoints = [];
+
+window.onload = function() {
+
+	// Call out for Octopus Agile Tarriffs Dataset
+	$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/", tariffsCallback);	
+
+}
+
+function tariffsCallback(data) {		
+	
+	tariffDPS.push("<input id=\"A\" type=\"button\" value=\"", data.single_register_electricity_tariffs._A.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"B\" type=\"button\" value=\"", data.single_register_electricity_tariffs._B.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._B.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"C\" type=\"button\" value=\"", data.single_register_electricity_tariffs._C.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._C.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"D\" type=\"button\" value=\"", data.single_register_electricity_tariffs._D.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._D.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"E\" type=\"button\" value=\"", data.single_register_electricity_tariffs._E.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._E.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"F\" type=\"button\" value=\"", data.single_register_electricity_tariffs._F.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._F.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"G\" type=\"button\" value=\"", data.single_register_electricity_tariffs._G.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._G.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"H\" type=\"button\" value=\"", data.single_register_electricity_tariffs._H.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._H.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"J\" type=\"button\" value=\"", data.single_register_electricity_tariffs._J.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._J.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"K\" type=\"button\" value=\"", data.single_register_electricity_tariffs._K.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._K.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"L\" type=\"button\" value=\"", data.single_register_electricity_tariffs._L.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._L.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"M\" type=\"button\" value=\"", data.single_register_electricity_tariffs._M.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._M.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"N\" type=\"button\" value=\"", data.single_register_electricity_tariffs._N.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._N.direct_debit_monthly.code, "\');\" /><br />");
+	tariffDPS.push("<input id=\"P\" type=\"button\" value=\"", data.single_register_electricity_tariffs._P.direct_debit_monthly.code, "\" onclick=\"getTariffData(\'", data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, "\',\'", data.single_register_electricity_tariffs._P.direct_debit_monthly.code, "\');\" />");	
+	
+	/* never build an HTML string in a loop, load to array then join with emptystring */
+	$("#divTariffs").html(tariffDPS.join(''));
+}
+
+function getTariffData(endpoint, tariff) {
+
+	// re-initialise the chart object
+	chart = null;
+	dataPoints = [];
+	//alert(endpoint);
+	//alert(tariff);
+
+	chart = new CanvasJS.Chart("divChart", {
+		animationEnabled: true,
+		zoomEnabled: true,
+		exportEnabled: true,
+		theme: "light1",
+		title: {
+			text: "Tariff: " + tariff
+		},
+		axisX:{
+			crosshair: {
+				enabled: true,
+				snapToDataPoint: true,
+				stripLines: [{
+				value: 3,
+				label: "Now",
+				labelFontColor: "#808080",
+				labelAlign: "near"
+				}]
+			}		
+		},
+		axisY: {
+			title: "pence per kWh",
+			includeZero: false,
+			crosshair: {
+				enabled: true,
+				snapToDataPoint: true,
+				labelFormatter: function(e) {
+					return CanvasJS.formatNumber(e.value, "##0.000") + " p/kWh";
+				}
+			}
+		},
+		data: [{
+			type: "area",
+			markerSize: 12,
+			xValueType: "dateTime",
+			xValueFormatString: "DD MMM hh:mm TT",
+			valueFormatString: "##0",
+			connectNullData: true,
+			dataPoints: dataPoints
+		}]
+	});
+
+	// Call out for specific Agile Tariff Dataset
+	$.getJSON(endpoint, tariffCallback);	
+
+}
+
+function tariffCallback(data) {			
+	var i, len;
+	for (len = data.results.length, i=0; i<len; i++) {	
+		if (data.results[i].value_inc_vat <= 0) {
+			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "cross", markerColor: "tomato" });	
+		}else{
+			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "none"});
+		}
+	} 
+	chart.render(); 
+}
--- octo.js
+++ octo.js
@@ -5,11 +5,6 @@ var tariffDPS = [];
 
 window.onload = function() {
 
-// Call out for Octopus Agile Tarriffs Dataset
-$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/", callback(data, 0));	
-
-}
-
 chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	zoomEnabled: true,
@@ -53,41 +48,18 @@ chart = new CanvasJS.Chart("chartContainer", {
 });
 
 // Call out for Octopus Agile Tarriff Dataset
-$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-A/standard-unit-rates", callback(data, 1));	
+$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-A/standard-unit-rates", callback);	
 
+}
 
-function callback(data, opt) {	
-	
-	if (opt > 0) {
-		
-		var i, len;
-		for (len = data.results.length, i=0; i<len; i++) {	
-			if (data.results[i].value_inc_vat <= 0) {
-				dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "cross", markerColor: "tomato" });	
-			}else{
-				dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "none"});
-			}
-		} 
-		chart.render(); 
-
-	}else{
-
-		tariffDPS.push(data.single_register_electricity_tariffs._A.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._A.direct_debit_monthly.code, data.single_register_electricity_tariffs._A.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._B.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._B.direct_debit_monthly.code, data.single_register_electricity_tariffs._B.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._C.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._C.direct_debit_monthly.code, data.single_register_electricity_tariffs._C.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._D.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._D.direct_debit_monthly.code, data.single_register_electricity_tariffs._D.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._E.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._E.direct_debit_monthly.code, data.single_register_electricity_tariffs._E.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._F.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._F.direct_debit_monthly.code, data.single_register_electricity_tariffs._F.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._G.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._G.direct_debit_monthly.code, data.single_register_electricity_tariffs._G.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._H.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._H.direct_debit_monthly.code, data.single_register_electricity_tariffs._H.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._J.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._J.direct_debit_monthly.code, data.single_register_electricity_tariffs._J.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._K.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._K.direct_debit_monthly.code, data.single_register_electricity_tariffs._K.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._L.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._L.direct_debit_monthly.code, data.single_register_electricity_tariffs._L.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._M.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._M.direct_debit_monthly.code, data.single_register_electricity_tariffs._M.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._N.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._N.direct_debit_monthly.code, data.single_register_electricity_tariffs._N.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-		tariffDPS.push(data.single_register_electricity_tariffs._P.direct_debit_monthly.links[1].href, data.single_register_electricity_tariffs._P.direct_debit_monthly.code, data.single_register_electricity_tariffs._P.direct_debit_monthly.standard_unit_rate_inc_vat + "<br />");
-			
-		/* uncomment to debug */
-		$("#divTariffs").html(tariffDPS.join(''));
-	}	
+function callback(data) {			
+	var i, len;
+	for (len = data.results.length, i=0; i<len; i++) {	
+		if (data.results[i].value_inc_vat <= 0) {
+			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "cross", markerColor: "tomato" });	
+		}else{
+			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "none"});
+		}
+	} 
+	chart.render(); 
 }

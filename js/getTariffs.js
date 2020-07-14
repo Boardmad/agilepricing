47135dec580cc4bab219048fdde21f3bfe35b79f
Scott Brown <scottbrown@boardmad.com>
2020-07-14 19:52:56 +0100

    cleaned up chart config code

--- js/getTariffs.js
+++ js/getTariffs.js
@@ -8,19 +8,17 @@ window.onload = function() {
 	// Call out for Octopus Agile Tarriffs Dataset
 	$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/", tariffsCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariffs completed"); })
-	//.fail(function(jqXHR, textStatus, errorThrown) { $("#divOutput").html($("#divOutput").html+"< br />Failed to retrieve agile tariffs data" + textStatus); })
-	// Note trailing semi !
-	;
+	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve agile tariffs data" + textStatus); });
 }
 
 function tariffsCallback(data) {		
+	//Tariff identifiers
 	var iterArr = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P"];
 	var i, len;
-	for (len = iterArr.length, i=0; i<len; i++) {	
-		
+
+	for (len = iterArr.length, i=0; i<len; i++) {			
 		// Ugly - no obvious way to programatically retrieve the tariffs from the all_tariffs JSON
 		tariffDPS.push("<input id=\""+iterArr[i]+"\" type=\"button\" value=\"", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\" onclick=\"getTariffData(\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.links[1].href"), "\',\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\');\" /><br />");
-	
 	}	
 	/* never build an HTML string in a loop, load to array then join with emptystring */
 	$("#divTariffs").html(tariffDPS.join(''));
@@ -36,53 +34,23 @@ function getTariffData(endpoint, tariff) {
 		zoomEnabled: true,
 		exportEnabled: true,
 		theme: "light1",
-		title: {
-			text: "Tariff: " + tariff
-		},
+		title: { text: "Tariff: " + tariff },
 		axisX:{
-			stripLines: [{
-				value: Date.now() ,
-				label: "Now",
-				labelFontColor: "#808080",
-				labelAlign: "near",
-				thickness: 3,
-				}],
-			crosshair: {
-				enabled: true,
-				snapToDataPoint: true,
-			}		
-		},
+			stripLines: [{ value: Date.now(), label: "Now",	labelFontColor: "#808080", labelAlign: "near",	thickness: 3 }],
+			crosshair: { enabled: true,	snapToDataPoint: true }		
+			},
 		axisY: {
-			minimum: 0,
-			interval: 5,
-			title: "pence per kWh",
-			includeZero: false,
-			crosshair: {
-				enabled: true,
-				snapToDataPoint: true,
-				labelFormatter: function(e) {
-					return CanvasJS.formatNumber(e.value, "##0.000") + " p/kWh";
-				}
+			minimum: 0,	interval: 5, title: "pence per kWh", includeZero: false, crosshair: 
+				{ enabled: true, snapToDataPoint: true,	labelFormatter: function(e) { return CanvasJS.formatNumber(e.value, "##0.000") + " p/kWh"; }
 			}
 		},
-		data: [{
-			type: "area",
-			markerSize: 12,
-			xValueType: "dateTime",
-			xValueFormatString: "DD MMM hh:mm TT",
-			valueFormatString: "##0",
-			connectNullData: true,
-			dataPoints: dataPoints
-		}]
+		data: [{ type: "area", markerSize: 12, xValueType: "dateTime", xValueFormatString: "DD MMM hh:mm TT", valueFormatString: "##0",	connectNullData: true,	dataPoints: dataPoints }]
 	});
 
-	//alert("Getting " + endpoint)
 	// Call out for specific Agile Tariff Dataset
 	$.getJSON(endpoint, tariffCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariff completed"); })
-	//.fail(function(jqXHR, textStatus, errorThrown) { $("#divOutput").html($("#divOutput").html+"< br />Failed to retrieve tariff data" + textStatus); })
-	// Note trailing semi!
-	;
+	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve hourly pricing data" + textStatus); }) ;
 }
 
 function tariffCallback(data) {			

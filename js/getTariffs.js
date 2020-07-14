166b7ae9c1fe60f7c069c137c447779f5f3f33b7
Scott Brown <scottbrown@boardmad.com>
2020-07-14 20:03:44 +0100

    cleaned up error alerting on JSON fail

--- js/getTariffs.js
+++ js/getTariffs.js
@@ -4,31 +4,16 @@ var tariffDPS = [];
 var dataPoints = [];
 
 window.onload = function() {
-
 	// Call out for Octopus Agile Tarriffs Dataset
 	$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/", tariffsCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariffs completed"); })
 	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve agile tariffs data" + textStatus); });
 }
 
-function tariffsCallback(data) {		
-	//Tariff identifiers
-	var iterArr = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P"];
-	var i, len;
-
-	for (len = iterArr.length, i=0; i<len; i++) {			
-		// Ugly - no obvious way to programatically retrieve the tariffs from the all_tariffs JSON
-		tariffDPS.push("<input id=\""+iterArr[i]+"\" type=\"button\" value=\"", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\" onclick=\"getTariffData(\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.links[1].href"), "\',\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\');\" /><br />");
-	}	
-	/* never build an HTML string in a loop, load to array then join with emptystring */
-	$("#divTariffs").html(tariffDPS.join(''));
-}
-
-function getTariffData(endpoint, tariff) {
+function drawChart(endpoint, tariff) {
 	// re-initialise the chart object
 	chart = null;
 	dataPoints = [];
-
 	chart = new CanvasJS.Chart("divChart", {
 		animationEnabled: true,
 		zoomEnabled: true,
@@ -46,11 +31,23 @@ function getTariffData(endpoint, tariff) {
 		},
 		data: [{ type: "area", markerSize: 12, xValueType: "dateTime", xValueFormatString: "DD MMM hh:mm TT", valueFormatString: "##0",	connectNullData: true,	dataPoints: dataPoints }]
 	});
-
 	// Call out for specific Agile Tariff Dataset
 	$.getJSON(endpoint, tariffCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariff completed"); })
 	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve hourly pricing data" + textStatus); }) ;
+}
+
+/* getJSON callbacks functions */
+function tariffsCallback(data) {		
+	//Tariff identifiers
+	var iterArr = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P"];
+	var i, len;
+	for (len = iterArr.length, i=0; i<len; i++) {			
+		// Ugly - no obvious way to programatically retrieve the tariffs from the all_tariffs JSON
+		tariffDPS.push("<input id=\""+iterArr[i]+"\" type=\"button\" value=\"", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\" onclick=\"drawChart(\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.links[1].href"), "\',\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\');\" /><br />");
+	}	
+	/* never build an HTML string in a loop, load to array then join with emptystring */
+	$("#divTariffs").html(tariffDPS.join(''));
 }
 
 function tariffCallback(data) {			

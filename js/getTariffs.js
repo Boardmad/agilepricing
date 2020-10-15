f22b4e20fd095b60d165d5b9ec514ef2010732b3
Scott Brown <scottbrown@boardmad.com>
2020-10-15 12:16:09 +0100

    Additional arrays for smart meter data

--- js/getTariffs.js
+++ js/getTariffs.js
@@ -1,14 +1,34 @@
 
 var chart = null;
-var tariffDPS = [];
-var dataPoints = [];
-var page = 1;
+var tariffDPS = []; // An array of strings making up the tariff buttons and their respective getJSON calls 
+var dataPoints = [];// An array of datapoints for the retrieved tariff 
+var elecData = [];  // An array of datapoints for our electricity endpoint
+var gasData = [];	// An array of datapoints for our gas meter
+//var paging = 1;		// An interator for moving through data pages...not yet implemente
+
+
+// On window load retrieve all current Agile Tariff half hourly rates and render
+// a series of input buttons with the relevant call for that tariff.  This triggers
+// an asynch call to get the specific tariff data and then renders that chart data
 
 window.onload = function() {
 	// Call out for Octopus Agile Tarriffs Dataset
 	$.getJSON("https://api.octopus.energy/v1/products/AGILE-18-02-21/", tariffsCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariffs completed"); })
 	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve agile tariffs data" + textStatus); });
+}
+
+/* getJSON Tariffs callback functions */
+function tariffsCallback(data) {		
+	//Tariff identifiers
+	var iterArr = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P"];
+	var i, len;
+	for (len = iterArr.length, i=0; i<len; i++) {			
+		// Ugly - no obvious way to programatically retrieve the tariffs from the all_tariffs JSON
+		tariffDPS.push("<input id=\""+iterArr[i]+"\" type=\"button\" value=\"", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\" onclick=\"drawChart(\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.links[1].href"), "\',\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\');\" /><br />");
+	}	
+	/* never build an HTML string in a loop, load to array then join with emptystring */
+	$("#divTariffs").html(tariffDPS.join(''));
 }
 
 function drawChart(endpoint, tariff) {
@@ -36,19 +56,10 @@ function drawChart(endpoint, tariff) {
 	$.getJSON(endpoint, tariffCallback)	
 	//.done(function() { $("#divOutput").html($("#divOutput").html+"< br />getJSON Tariff completed"); })
 	.fail(function(jqXHR, textStatus, errorThrown) { alert("Failed to retrieve hourly pricing data" + textStatus); }) ;
-}
 
-/* getJSON callbacks functions */
-function tariffsCallback(data) {		
-	//Tariff identifiers
-	var iterArr = ["A","B","C","D","E","F","G","H","J","K","L","M","N","P"];
-	var i, len;
-	for (len = iterArr.length, i=0; i<len; i++) {			
-		// Ugly - no obvious way to programatically retrieve the tariffs from the all_tariffs JSON
-		tariffDPS.push("<input id=\""+iterArr[i]+"\" type=\"button\" value=\"", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\" onclick=\"drawChart(\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.links[1].href"), "\',\'", eval("data.single_register_electricity_tariffs._"+iterArr[i]+".direct_debit_monthly.code"), "\');\" /><br />");
-	}	
-	/* never build an HTML string in a loop, load to array then join with emptystring */
-	$("#divTariffs").html(tariffDPS.join(''));
+	// calls to retrieve meter usage data goes here
+
+
 }
 
 function tariffCallback(data) {			
@@ -65,7 +76,7 @@ function tariffCallback(data) {
 		}
 
 		//evaluate if price point is less that zero and style accordingly
-		if (data.results[i].value_inc_vat <= 6) {
+		if (data.results[i].value_inc_vat <= 5) {
 			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "cross", markerColor: "tomato" });	
 		}else{
 			dataPoints.push({x: new Date(data.results[i].valid_to),	y: data.results[i].value_inc_vat, markerType: "none"});
@@ -74,6 +85,7 @@ function tariffCallback(data) {
 
 	//Update price range label
 	$("#divRange").html("Price Range: " +lo+" - "+hi+ "p per kWh");
+	
 	//Render the chart object
 	chart.render(); 
 }
